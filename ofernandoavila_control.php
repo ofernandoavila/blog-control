7a9fe48f11d815a109154ffa243405d665169c0d
Fernando Avila <fernandoavilajunior@gmail.com>
2025-01-29 17:33:11 -0300

    Support to post thumbnail

--- ofernandoavila_control.php
+++ ofernandoavila_control.php
@@ -29,3 +29,9 @@ add_action( 'rest_api_init', function() {
         ]
     );
 });
+
+function theme_setup(){
+    add_theme_support('post-thumbnails');
+}
+
+add_action('after_setup_theme', 'theme_setup');
